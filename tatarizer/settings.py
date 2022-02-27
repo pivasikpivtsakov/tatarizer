a0b06b937ad5d706b33260fb6818da3d6b2b7dbf
pivasikpivtsakov <pivasikpivtsakov@github.com>
2022-02-27 20:23:44 +0500

    db

--- tatarizer/settings.py
+++ tatarizer/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import environ
-
+import dj_database_url
+from django.conf.global_settings import DATABASES
 
 env = environ.Env()
 env.read_env()
@@ -85,12 +86,7 @@ WSGI_APPLICATION = 'tatarizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
