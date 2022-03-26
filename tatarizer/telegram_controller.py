5291c603f3a69d3da94818f0ebf4677c49efae56
pivasikpivtsakov <pivasikpivtsakov@github.com>
2022-03-27 00:10:45 +0300

    fixing again

--- tatarizer/telegram_controller.py
+++ tatarizer/telegram_controller.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 
 
 class TelegramController(APIView):
-    def post(self):
+    def post(self, request):
         return 'ok'
 
-    def get(self):
+    def get(self, request):
         return 'hello this is api root'
