7d4d99645d52fa7ffc8696fea8851e76aa974a65
pivasikpivtsakov <pivasikpivtsakov@github.com>
2022-04-03 23:54:17 +0300

    fix for ready() called twice

--- tg_main/apps.py
+++ tg_main/apps.py
@@ -1,5 +1,8 @@
-from django.apps import AppConfig
+import os
+
 import httpx as http
+from django.apps import AppConfig
+
 from .telegram_urls import TGURL_SETWEBHOOK, API_ROOT
 
 
@@ -9,6 +12,7 @@ class TgMainConfig(AppConfig):
 
     # startup hook
     def ready(self):
-        with http.Client() as client:
-            response = client.post(TGURL_SETWEBHOOK, data={'url': API_ROOT})
-            print('setWebhook result: ', response.json())
+        if os.environ.get('RUN_MAIN') != 'true':
+            with http.Client() as client:
+                response = client.post(TGURL_SETWEBHOOK, data={'url': API_ROOT})
+                print('setWebhook result: ', response.json())
